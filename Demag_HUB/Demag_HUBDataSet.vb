8e631c73e246bdfc0af97eb381615da98035db78
Maximilian Braun <MaximilianBraun@outlook.com>
2018-06-17 10:04:47 +0200

    Created = date.Now

---
+++ Demag_HUB/Demag_HUBDataSet.vb
@@ -0,0 +1,7 @@
+﻿Partial Class dsDemag_HUB
+    Partial Public Class dsShipmentsDataTable
+        Private Sub DeineTabelleDataTable_TableNewRow(sender As Object, e As DataTableNewRowEventArgs) Handles Me.TableNewRow
+            DirectCast(e.Row, dsShipmentsRow).Created = Date.Now
+        End Sub
+    End Class
+End Class
